c2adcbcac694abb5ebbe931a88cd24f6d97be236
AdrianosullivanUL <16230124@studentmail.ul.ie>
2018-09-18 12:57:57 +0100

    First Working Version

--- HelloButton/app/src/main/java/com/example/adria/hellobutton/MainActivity.java
+++ HelloButton/app/src/main/java/com/example/adria/hellobutton/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.adria.hellobutton;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView mMessageTextView;
+    private int mCounter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMessageTextView = findViewById(R.id.message_textview);
+        //mMessageTextView.setText("Changed name");
+        //Log.d("HB","This is a log cat log.");
+
+        final Button resetButton = findViewById(R.id.reset_button);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCounter = 0;
+                updateView();
+            }
+        });
+    }
+    public void handleDecrement (View view)
+    {
+        mCounter--;
+        updateView();
+    }
+    public void handleIncrement (View view)
+    {
+        mCounter++;
+        updateView();
+    }
+    private void updateView()
+    {
+        mMessageTextView.setText((getString(R.string.message_format, mCounter)));
     }
 }
