bd631b4b29250475c87ad241d1f88c08c8f073f0
AdrianosullivanUL <16230124@studentmail.ul.ie>
2018-10-16 21:09:22 +0100

    added code to add button & upgraded android studio

--- PointOfSale/app/src/main/java/ie/ul/adrianosullivan/pointofsale/MainActivity.java
+++ PointOfSale/app/src/main/java/ie/ul/adrianosullivan/pointofsale/MainActivity.java
@@ -8,24 +8,44 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mName_text;
+    private TextView mQuantity_text;
+    private TextView mDate_text;
+    private Item mCurrentItem;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mName_text = findViewById(R.id.name_text);
+        mQuantity_text = findViewById(R.id.quantity_text);
+        mDate_text = findViewById(R.id.date_text);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            // for now show item on screen
+            mCurrentItem = Item.getDefaultItem();
+            showCurrentItem();
             }
         });
+    }
+
+    private void showCurrentItem() {
+        mName_text.setText(mCurrentItem.getName());
+        mQuantity_text.setText(getString(R.string.quantity_format,
+                mCurrentItem.getQuantity()));
+        mDate_text.setText(getString(R.string.date_format, mCurrentItem.getDeliveryDateString()));
     }
 
     @Override
