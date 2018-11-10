6f2329460d59fbf7cc99bb9df39cf25f16491d61
AdrianosullivanUL <16230124@studentmail.ul.ie>
2018-11-10 19:29:46 +0000

    add to firebase

--- MovieQuotes/app/src/main/java/ie/ul/adrianosullivan/moviequotes/MainActivity.java
+++ MovieQuotes/app/src/main/java/ie/ul/adrianosullivan/moviequotes/MainActivity.java
@@ -1,9 +1,11 @@
 package ie.ul.adrianosullivan.moviequotes;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -12,6 +14,8 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -39,20 +43,7 @@ public class MainActivity extends AppCompatActivity {
         // temp firebase test
         final FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-/*        db.collection("movie_quotes")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d(TAG, document.getId() + " => " + document.getData());
-                            }
-                        } else {
-                            Log.w(TAG, "Error getting documents.", task.getException());
-                        }
-                    }
-                });*/
+
 
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -68,17 +59,32 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "Added new quote to firestore", Snackbar.LENGTH_LONG)
                         .show();
 
-                // Create a new user with a first and last name
-                Map<String, Object> mq = new HashMap<>();
-                mTempCounter++;
-                mq.put("quote", "Quote #" + mTempCounter);
-                mq.put("movie", "Movie #" + mTempCounter);
-
-
-// Add a new document with a generated ID
-                db.collection("movie_quotes").add(mq);
+                showAddDialog();
             }
         });
+    }
+
+    private void showAddDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        View view = getLayoutInflater().inflate(R.layout.moviequote_dialog,null,false);
+        builder.setView(view);
+
+        final TextView quoteEditText = view.findViewById(R.id.dialog_quote_edittext);
+        final TextView movieEditText = view.findViewById(R.id.dialog_movie_edittext);
+
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Map<String, Object> mq = new HashMap<>();
+                mq.put(Constants.KEY_QUOTE, quoteEditText.getText().toString() );
+                mq.put(Constants.KEY_MOVIE, movieEditText.getText().toString());
+FirebaseFirestore.getInstance().collection(Constants.COLLECTION_PATH).add(mq);
+
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel,null);
+        builder.show();
     }
 
     @Override
